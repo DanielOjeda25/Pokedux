d37321a2d2b803994bbfad1a6c9bc636221a6896
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-15 12:27:03 -0300

    se agregaron los estilos en linea nuevamente

--- src/Components/DarkButton.jsx
+++ src/Components/DarkButton.jsx
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { useState, useEffect } from 'react'
-import { DarkButton as Db } from '../styles'
 
 const DarkButton = () => {
   const [theme, setTheme] = useState(null)
@@ -34,11 +33,11 @@ const DarkButton = () => {
     <button
       type='button'
       onClick={handleThemeSwitch}
-      className={`${Db.button}`}>
+      className='fixed z-10 right-3 top-2 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 transition-all duration-150'>
       {theme === 'dark' ? (
-        <FontAwesomeIcon icon={faSun} className={`${Db.FontIcon}`}/>
+        <FontAwesomeIcon icon={faSun} className='w-5 h-5' />
       ) : (
-        <FontAwesomeIcon icon={faMoon} className={`${Db.FontIcon}`}/>
+        <FontAwesomeIcon icon={faMoon} className='w-5 h-5' />
       )}
     </button>
   )
--- src/Components/Searcher.jsx
+++ src/Components/Searcher.jsx
@@ -1,27 +1,28 @@
 import logo from '../assets/pokeLogosvg.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { SearchBar } from '../styles'
 
 const Searcher = () => {
   return (
-    <header className={`${SearchBar.header}`}>
-      <div className={`${SearchBar.div1}`}>
-        <img src={logo} className={`${SearchBar.img}`} />
+    <header className='flex justify-center items-center h-24 w-full flex-col'>
+      <div className='aspect-w-16 aspect-h-9 flex justify-center items-center mt-8 mb-3'>
+        <img src={logo} className='xsm:w-2/3' />
       </div>
-      <div className={`${SearchBar.div2}`}>
-        <div className={`${SearchBar.div3}`}>
+      <div className='relative flex justify-center items-center w-full xl:w-3/6'>
+        <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none flex-none hover:text-slate-400 '>
           <FontAwesomeIcon
             icon={faMagnifyingGlass}
-            className={`${SearchBar.FavIcon}`}
+            className='dark:text-white z-10 hover:cursor-pointer'
           />
         </div>
         <input
           type='search'
           placeholder='Search pokemons'
-          className={`${SearchBar.input}`}
+          className='block p-4 pl-10 w-full text-sm text-gray-900 bg-slate-100 rounded-lg border border-gray-300 focus:ring-gray-400 focus:border-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
         />
-        <button type='submit' className={`${SearchBar.button}`}>
+        <button
+          type='submit'
+          className='text-white absolute right-2.5 bottom-2.5 bg-violet-500 hover:bg-violet-600  focus:ring-4 focus:outline-none focus:bg-violet-700 font-medium rounded-lg text-sm px-4 py-2 dark:bg-violet-500 dark:hover:bg-violet-600  dark:focus:bg-violet-700 '>
           Search
         </button>
       </div>
