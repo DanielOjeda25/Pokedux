b4ef5ddd22f71125f19f740466b02bdf476c0c2a
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-14 21:49:42 -0300

    falta refactorizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useEffect } from 'react'
 import './App.css'
+import { connect } from 'react-redux'
+
+import getPokemons from './Api/getPokemons'
+import { setPokemons as setPokemonsActions } from './Actions'
 import PokemonList from './Components/PokemonList'
 import Searcher from './Components/Searcher'
+import DarkButton from './Components/DarkButton'
 
-function App() {
+function App({ pokemons, setPokemons }) {
+  useEffect(() => {
+    const FetchPokemons = async () => {
+      const pokemonsList = await getPokemons()
+      setPokemons(pokemonsList)
+    }
+    FetchPokemons()
+  })
+
   return (
-    <div>
+    <div className='dark:bg-gray-800 font-Acme font-normal tracking-widest'>
+      <DarkButton />
       <Searcher />
-      <PokemonList />
+      <PokemonList pokemons={pokemons} />
     </div>
   )
 }
 
-export default App
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (values) => dispatch(setPokemonsActions(values)),
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App)
